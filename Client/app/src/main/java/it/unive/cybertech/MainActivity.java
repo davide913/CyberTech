3c2818de61a76cdfe58da0f8fb4aaee2ae2d40ba
877962 <877962@stud.unive.it>
2021-10-18 15:42:38 +0200

    Suddivisione in package per deliverable

--- Client/app/src/main/java/it/unive/cybertech/MainActivity.java
+++ Client/app/src/main/java/it/unive/cybertech/MainActivity.java
@@ -2,6 +2,7 @@ package it.unive.cybertech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.gestioneCovid).setOnClickListener(view -> {
+            startActivity(new Intent(this, it.unive.cybertech.gestione_covid.HomePage.class));
+        });
+
+        findViewById(R.id.assistenza).setOnClickListener(view -> {
+            startActivity(new Intent(this, it.unive.cybertech.assistenza.HomePage.class));
+        });
+
+        findViewById(R.id.noleggio).setOnClickListener(view -> {
+            startActivity(new Intent(this, it.unive.cybertech.noleggio.HomePage.class));
+        });
     }
 }
---
+++ Client/app/src/main/java/it/unive/cybertech/noleggio/HomePage.java
@@ -0,0 +1,16 @@
+package it.unive.cybertech.noleggio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.unive.cybertech.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_noleggio);
+    }
+}
