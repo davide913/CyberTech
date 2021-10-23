34356d459516bc431bb85d157246155095a2c34b
EnricoDeZorzi <876903@stud.unive.it>
2021-10-23 12:32:23 +0200

    Implementazione grafica terza schermata
    Completate le grafiche di base delle 3 schermate principali per la sezione Covid 19, aggiunta listView delle segnalazioni ricevute

--- Client/app/src/main/java/it/unive/cybertech/gestione_covid/PosReportedFragment.java
+++ Client/app/src/main/java/it/unive/cybertech/gestione_covid/PosReportedFragment.java
@@ -7,8 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import it.unive.cybertech.R;
+import it.unive.cybertech.gestione_covid.adapters.CustomSignReceivedAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +68,43 @@ public class PosReportedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pos_reported, container, false);
+        View v = inflater.inflate(R.layout.fragment_pos_reported, container, false);
+
+        Boolean var = false;
+
+        ImageView imageView = v.findViewById(R.id.imageView_PosReported);
+        TextView textView = v.findViewById(R.id.TextView_PosReported);
+        TextView textView1 = v.findViewById(R.id.textView_UltimeSegnalazioni);
+        ListView listView = v.findViewById(R.id.ListView_signReported);
+
+        if(var){
+            imageView.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.INVISIBLE);
+            listView.setVisibility(View.VISIBLE);
+            textView1.setVisibility(View.VISIBLE);
+            ArrayList<String> sign = new ArrayList<>();
+            ArrayAdapter<String> adapter;
+
+            //Prove ListView con Adapter
+            sign.add("rawlist1");
+            sign.add("rawlist2");
+            sign.add("rawlist3");
+            sign.add("rawlist4");
+            sign.add("rawlist5");
+            sign.add("rawlist6");
+
+            adapter = new CustomSignReceivedAdapter(getContext(), 0, sign);
+
+            listView.setAdapter(adapter);
+        }
+        else{
+            imageView.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.VISIBLE);
+            listView.setVisibility(View.INVISIBLE);
+            textView1.setVisibility(View.INVISIBLE);
+        }
+
+        return v;
     }
+
 }
---
+++ Client/app/src/main/java/it/unive/cybertech/gestione_covid/adapters/CustomSignReceivedAdapter.java
@@ -0,0 +1,44 @@
+package it.unive.cybertech.gestione_covid.adapters;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unive.cybertech.R;
+
+public class CustomSignReceivedAdapter extends ArrayAdapter {
+    private static final String TAG = "CustomOrderArrayAdapterTrader";
+    private Context context;
+    private List<String > sign;
+
+    public CustomSignReceivedAdapter(@NonNull Context context, int resource, ArrayList<String> sign) {
+        super(context, resource, sign);
+        this.context = context;
+        this.sign = sign;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public View getView(int position, View convertView, ViewGroup parent){
+        Log.d(TAG, "call getView");
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        @SuppressLint({"ViewHolder", "InflateParams"}) View view = inflater.inflate(R.layout.raw_list_sign_reported, null);
+
+
+
+        TextView Nome = view.findViewById(R.id.nome_segnalatore);
+        TextView Cognome= view.findViewById(R.id.cognome_segnalatore);
+
+        return view;
+    }
+}
